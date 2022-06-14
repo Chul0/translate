90f2ffe80cc670527dfd80a5f4a32d7c467effd7
Sungchul Park <sungchulpark0@gmail.com>
2022-06-14 17:59:43 -0400

    wrapping up

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,19 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-    static contextType = LanguageContext; //this is how hook context to a class component.
-
+    renderSubmit(value) { //helper function 
+        return value === "english" ? "Submit" : "Voorleggen"
+    }
     render() {
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen'; //accessing to context and getting value. this.context
-        return <button className="ui button primary">{text}</button>;
+        return (
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        );
+        // With Consumer, you can access to the Context data
+        // Whenever you user Consumer, you always provide one child(is always going to be a function)
     }
 }
 
