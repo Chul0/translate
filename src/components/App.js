ae2468d4016254d27442165213098080ba975e9c
Sungchul Park <sungchulpark0@gmail.com>
2022-06-14 17:13:12 -0400

    language setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 
 class App extends React.Component {
+    state = { language: "english" };
+
+    onLanguageChange = language => {
+        this.setState({ language }) //same as {{ language:language }}
+    }
+
     render() {
-        return <div className="ui container">App</div>;
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')} /> 
+                    {/* every time user clicks this, callback onLanguageChange function and pass English */}
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+                {this.state.language}
+            </div>
+        );
     }
 }
 
