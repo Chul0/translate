da86b58b3a461bddb94c6389eecff5a3101b8e10
Sungchul Park <sungchulpark0@gmail.com>
2022-06-14 17:18:53 -0400

    Created components:button, field, userCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component {
     state = { language: "english" };
@@ -16,7 +17,7 @@ class App extends React.Component {
                     {/* every time user clicks this, callback onLanguageChange function and pass English */}
                     <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
                 </div>
-                {this.state.language}
+                <UserCreate />
             </div>
         );
     }
